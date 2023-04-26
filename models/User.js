d7c33c8d5c76f26352ab65b2ed86d26eec7cf17c
twqueen <tiffanyytsai@gmail.com>
2023-04-25 21:49:39 -0700

    updated user controller js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema(
     {
         username: { type: String, unique: true, trim: true, required: true },
-        email: { type: String, unique: true, required: true, match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],},
+        email: { type: String, unique: true, required: true, match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/], },
         thoughts: [
             {
                 type: Schema.Types.ObjectId,
@@ -15,7 +15,7 @@ const userSchema = new Schema(
                 type: Schema.Types.ObjectId,
                 ref: 'User',
             },
-        ],      
+        ],
     },
     {
         toJSON: {
